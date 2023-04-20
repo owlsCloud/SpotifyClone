20589ed8412375e32a99dac4a5c6adde8c5aae00
Gerardo Garza <owlscloud@gmail.com>
2023-04-20 03:09:01 -0500

    a

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.get("/callback", (req, res) => {
           expires_in,
         });
         console.log(`${FRONTEND_URI}`);
-        res.redirect(`http://${FRONTEND_URI}?${queryParams}`);
+        res.redirect(`${FRONTEND_URI}?${queryParams}`);
       } else {
         res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
       }
